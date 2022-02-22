268ef7de17380d91b1fc82332020aa3d2d07fc16
Ali Shah <s.ashah@outlook.com>
2022-02-22 00:05:21 -0500

    [Finished] exercise 4

--- exercise-4.py
+++ exercise-4.py
@@ -3,22 +3,19 @@ Exercise 4
 """
 
 # PART 1: Gather Information
-#
-# TODO: Gather information about the source of the error and paste your findings here. E.g.:
-# - What is the expected vs. the actual output?
-# - What error message (if any) is there?
-# - What line number is causing the error?
-# - What can you deduce about the cause of the error?
-
+# - The expected output is the index where the search target exists in the array. In this, index 4. The actual output is an error for maximum recursion depth exceeded.
+#   - The error means that there's a flaw in our routing logic that causes infinite recursion.
+# - The lines in our if-structure where we recursively call `binary_search()` are the lines that are causing the error.
+# - The cause of the error must be that the parameters being passed are not satisfying the condition for the recursion to terminate.
 
 # PART 2: State Assumptions
-#
-# TODO: State your assumptions here or say them out loud to your partner ...
-# Make sure to be SPECIFIC about what each of your assumptions is!
-# HINT: It may help to draw a picture to clarify what your assumptions are.
+# - We assume that each time we recurse, the mid-point of our search is half-way between the previous mid-point.
+# - In the lines where we called `binary_search()`, we assume that the new midpoint is different than the previous midpoint as we traverse through the indices.
+#   - This is the source of the bug. Eventually we hit a point where the midpoint is the same as the previous midpoint, causing infinite recursion.
+#   - This can be fixed by setting the `high` value to `mid - 1` if arr[mid] > element and by setting `low` to `mid + 1` if arr[mid] < element.
 
 def binary_search(arr, element, low=0, high=None):
-      """Returns the index of the given element within the array by performing a binary search."""
+    """Returns the index of the given element within the array by performing a binary search."""
     if high == None:
         high = len(arr) - 1
 
